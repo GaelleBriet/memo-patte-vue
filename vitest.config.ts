b791ef82435c1307ff0d0c46808b1641546202e4
Gaelle Briet <gaelle.briet@protonmail.com>
2026-08-23 10:11:19 +0200

    chore: update vitest.config.ts to include setup files and server dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import viteConfig from './vite.config.ts'
 
 export default mergeConfig(
   viteConfig,
@@ -9,6 +9,12 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      setupFiles: ['./vitest.setup.ts'],
+      server: {
+        deps: {
+          inline: ['vuetify'],
+        },
+      },
     },
   }),
 )
